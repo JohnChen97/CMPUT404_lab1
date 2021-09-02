bdc801c0e8534ece38db53fd5222fd56eb066869
JohnChen97 <chenjionghao@yahoo.com>
2021-09-02 17:31:08 -0600

    Upload the python script

---
+++ requests_version_script.py
@@ -0,0 +1,5 @@
+import requests, sys
+if __name__ == '__main__':
+	sys.stdout.write(f'the requests version is {requests.__version__} \n')
+	sys.stdout.write(f'Google homepage: {requests.get("https://www.google.com").text}')
+	sys.stdout.write(f'Google homepage: {requests.get("https://w3schools.com/python/demopage.htm").text}')
