7f73b08d11b980f6f425c34e5811b3ab832302d4
JohnChen97 <chenjionghao@yahoo.com>
2021-09-03 15:12:53 -0600

    Fixed some error

--- requests_version_script.py
+++ requests_version_script.py
@@ -6,8 +6,13 @@ if __name__ == '__main__':
     python_script = requests.get(
         "https://raw.githubusercontent.com/JohnChen97/CMPUT404_lab1/main/requests_version_script.py"
     )
-    sys.stdout.write(python_script.text)
-    #sys.stdout.write(python_script.content)
+    source_code = requests.get(
+        'https://github.com/JohnChen97/CMPUT404_lab1/blob/main/requests_version_script.py'
+    )
+    sys.stdout.write(
+        f'The source code of the python script from github is: {source_code.text}\n'
+    )
+    sys.stdout.write(f'Raw link: {python_script.text}\n')
 
     with open('python_script_github.py', 'w') as doc:
         doc.write(python_script.text)
