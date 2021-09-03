cfb9ee40675c82defc732fdb65d7956911363959
JohnChen97 <chenjionghao@yahoo.com>
2021-09-02 22:14:06 -0600

    Add a function to get python file from github

--- requests_version_script.py
+++ requests_version_script.py
@@ -1,5 +1,14 @@
 import requests, sys
 if __name__ == '__main__':
-	sys.stdout.write(f'the requests version is {requests.__version__} \n')
-	sys.stdout.write(f'Google homepage: {requests.get("https://www.google.com").text}')
-	
+    sys.stdout.write(f'the requests version is {requests.__version__} \n')
+    sys.stdout.write(
+        f'Google homepage: {requests.get("https://www.google.com").text}')
+    python_script = requests.get(
+        "https://raw.githubusercontent.com/JohnChen97/CMPUT404_lab1/main/requests_version_script.py"
+    )
+    sys.stdout.write(python_script.text)
+    #sys.stdout.write(python_script.content)
+
+    with open('python_script_github.py', 'w') as doc:
+        doc.write(python_script.text)
+        doc.close()
