928d1295cc8ec6f2d19f32f1b55593b14eee4cc1
JohnChen97 <chenjionghao@yahoo.com>
2021-09-10 15:17:58 -0600

    change raw link name

--- lab1_python_script.py
+++ lab1_python_script.py
@@ -4,10 +4,10 @@ if __name__ == '__main__':
     sys.stdout.write(
         f'Google homepage: {requests.get("https://www.google.com").text}')
     python_script = requests.get(
-        "https://raw.githubusercontent.com/JohnChen97/CMPUT404_lab1/main/requests_version_script.py"
+        "https://raw.githubusercontent.com/JohnChen97/CMPUT404_lab1/main/lab1_python_script.py"
     )
     source_code = requests.get(
-        'https://github.com/JohnChen97/CMPUT404_lab1/blob/main/requests_version_script.py'
+        'https://github.com/JohnChen97/CMPUT404_lab1/blob/main/lab1_python_script.py'
     )
     sys.stdout.write(
         f'The source code of the python script from github is: {source_code.text}\n'
